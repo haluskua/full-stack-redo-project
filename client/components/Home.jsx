b51037303e75265fd2183d24efab8fb184cec64f
Haluskua <haluskua.one@gmail.com>
2020-02-16 18:37:16 +1300

    added file into desktop monitor to work on it

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -8,16 +8,12 @@ class Home extends React.Component {
       <div className="container topMargin columns">
         <div className="column is-3">
           <h2 className="title is-4">Hello Kotare</h2>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
+  
         </div>
-        {/* <img
+        <img
           className="column is-8 is-offset-1"
           src="https://www.sheknows.com/wp-content/uploads/2018/08/horiz-cute-pets-computer_fd96wu.jpeg"
-        /> */}
+        />
       </div>
     );
   }
